f5818fd70615b345e315b9dcd11735ad1d546b36
joao <joao_mcm@hotmail.com>
2020-01-12 20:24:38 -0600

    added comments to 3rd person camera code

--- Robot_visualizer.py
+++ Robot_visualizer.py
@@ -47,19 +47,26 @@ def visualUpdateLoop():
         sensed_position = robot.getKlamptSensedPosition()
         vis_robot.setConfig(sensed_position)
         ## end effector is 42
+        # Adding the third person perspective. First, select a link that is aligned with the base
         EndLink = vis_robot.link(3)           # This link number should be the end effector link number
+        # get that link's T
         Tlink = EndLink.getTransform()
         # vis.add("Frame",Tlink)
+        # Do transforms to get it into Klampt format
         rot_link = so3.from_matrix(so3.matrix(Tlink[0]))
+        #add view angle of 45 degrees down
         rot_view = so3.from_matrix(so3.matrix(so3.from_axis_angle([[0,1,0],-np.pi/4])))
         inter_view = so3.mul(rot_link,rot_view)
+        #add a view angle to make sure the camera is upright
         rot_view_2 = so3.from_matrix(so3.matrix(so3.from_axis_angle([[0,0,1],-np.pi/2])))
         final_view = so3.mul(inter_view,rot_view_2)
+        #Make sure the camera arm moves together with the robot by rotating it along its axis.
         intermediate_dist = so3.apply(Tlink[0],[-3.05,0,3.6])
         # final_dist = (np.array(Tlink[1]) + np.array([-3.05,0,3.6])).tolist()
         final_dist = (np.array(Tlink[1]) + np.array(intermediate_dist)).tolist()
         
         # final_dist = so3.apply(final_view,new_dist)
+        # apply the new camera view
         print(final_dist)
         vp = vis.getViewport()
         camera = vp.camera
