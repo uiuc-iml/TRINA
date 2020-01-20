c8dd3d62eba38a8fccbe2c87f43eebf37e22e2cd
Fetch <liyuecheng27@gmail.com>
2020-01-20 14:56:51 -0600

    adding updated gripper controller code

--- Motion/gripperController.py
+++ Motion/gripperController.py
@@ -13,6 +13,7 @@ from reflex_msgs2.msg import Command
 from reflex_msgs2.msg import PoseCommand
 from reflex_msgs2.msg import VelocityCommand
 from reflex_msgs2.msg import Hand
+from reflex_msgs2.msg import Motor
 from reflex_msgs2.msg import FingerPressure
 from reflex_msgs2.srv import SetTactileThreshold, SetTactileThresholdRequest
 
@@ -25,14 +26,16 @@ MIN_MOTOR_SPEED = -3.5
 MAX_MOTOR_TRAVEL = 3.6
 MIN_MOTOR_TRAVEL = 0
 
+hand_state = Hand()
+
 class GripperController:
     def __init__(self):
-        self.command_pub = rospy.Publisher('/reflex_takktile2/command', Command, queue_size=1)
-        self.pos_pub = rospy.Publisher('/reflex_takktile2/command_position', PoseCommand, queue_size=1)
-        self.vel_pub = rospy.Publisher('/reflex_takktile2/command_velocity', VelocityCommand, queue_size=1)
+        self.command_pub = rospy.Publisher('/reflex_takktile2/command', Command, queue_size=5)
+        self.pos_pub = rospy.Publisher('/reflex_takktile2/command_position', PoseCommand, queue_size=5)
+        self.vel_pub = rospy.Publisher('/reflex_takktile2/command_velocity', VelocityCommand, queue_size=5)
         self.sub = rospy.Subscriber('/reflex_takktile2/hand_state', Hand, self.callback)
 
-        self.dt = 0.01
+        self.dt = 1.0/10.0
 
         self.sense_finger_set = [0.0, 0.0, 0.0, 0.0]
         self.command_finger_set = [0.0, 0.0, 0.0, 0.0]
@@ -63,17 +66,16 @@ class GripperController:
 
         # self.calibrate()
         while not rospy.is_shutdown() and (not self.exit):
+            print(self.sense_finger_set)
 
             if self.stop:
                 self.pos_pub.publish(VelocityCommand(f1 = 0.0, f2 = 0.0, f3 = 0.0, preshape = 0.0))
-            if self.command_type == "pose":
-                print(self.command_finger_set[0])
+            elif self.command_type == "pose":
                 self.pos_pub.publish(PoseCommand(f1 = self.command_finger_set[0], f2 = self.command_finger_set[1], f3 = self.command_finger_set[2], preshape = self.command_finger_set[3]))
-                print("why it doesn't publish")
-                self.command_type = None
+                #self.command_type = None
             elif self.command_type == "velocity":
                 self.vel_pub.publish(VelocityCommand(f1 = self.command_finger_set[0], f2 = self.command_finger_set[1], f3 = self.command_finger_set[2], preshape = self.command_finger_set[3]))
-                self.command_type = None
+                # self.command_type = None
 
             rate.sleep()
     def callback(self, data):
@@ -134,21 +136,23 @@ class GripperController:
         self.command_type = "velocity"
 
     def close(self):
-        self.command_finger_set[0] = 3.5
-        self.command_finger_set[1] = 3.5
-        self.command_finger_set[2] = 3.5
-        self.command_finger_set[3] = 2.0
+        self.command_finger_set[0] = 3.0
+        self.command_finger_set[1] = 3.0
+        self.command_finger_set[2] = 3.0
+        self.command_finger_set[3] = 1.0
         self.command_type = "pose"
 
     def open(self):
         self.command_finger_set[0] = 0.0
         self.command_finger_set[1] = 0.0
         self.command_finger_set[2] = 0.0
-        self.command_finger_set[3] = 2.0
+        self.command_finger_set[3] = 1.0
         self.command_type = "pose"
 
 
-
+####################################
+###  probably won't be used now  ###
+####################################
     def calibrate(self):
         rospy.wait_for_service('/reflex_takktile2/calibrate_fingers')
         calibrate_fingers = rospy.ServiceProxy('/reflex_takktile2/calibrate_fingers', Empty)
@@ -157,6 +161,9 @@ class GripperController:
         calibrate_fingers()
         calibrate_tactile()
 
+####################################
+###  probably won't be used now  ###
+####################################
     def set_threshold(self, x):
         set_tactile_threshold = rospy.ServiceProxy('/your_gripper/set_tactile_threshold', SetTactileThreshold)
         set_tactile_threshold(x)
@@ -164,7 +171,8 @@ class GripperController:
 
 
     def is_open(self):
-        if sense_finger_set[0] == 0.0 and sense_finger_set[1] == 0.0 and sense_finger_set[2] == 0.0 and sense_finger_set[3] == 0.0:
+        print("is_open(): ", self.sense_finger_set)
+        if self.sense_finger_set[0] == 0.0 and self.sense_finger_set[1] == 0.0 and self.sense_finger_set[2] == 0.0:
             return 1
         else:
             return 0
@@ -173,7 +181,7 @@ class GripperController:
         return self.contact
 
     def sensed_finger_positions(self):
-        return sense_finger_set
+        return self.sense_finger_set
 
 
 
@@ -200,10 +208,47 @@ class GripperController:
 
     def new_state(self):
         return self.new_state
-# 
-# if __name__ == "__main__":
-#     con = GripperController()
-#
-#     # con.start()
-#     con.pos_pub.publish(PoseCommand(f1 = 0.0, f2 = 0.0, f3 = 0.0, preshape = 2.0))
-#     # con.setPose((1.0, 1.0,1.0,1.0))
+
+def hand_state_cb(data):
+    global hand_state
+    hand_state = data
+    #print(data)
+
+if __name__ == "__main__":
+    con = GripperController()
+    con.start()
+    while True:
+        con.close()
+        rospy.sleep(2)
+        con.open()
+        rospy.sleep(2)
+        print(con.is_open())
+    # con.setPose([0.0, 0.0, 0.0, 0.0])
+    # con.setPose([1.0, 1.0, 1.0, 1.0])
+    # rospy.init_node('ExampleHandNode')
+    #
+    # # Services can automatically call hand calibration
+    # calibrate_fingers = rospy.ServiceProxy('/reflex_takktile2/calibrate_fingers', Empty)
+    # calibrate_tactile = rospy.ServiceProxy('/reflex_takktile2/calibrate_tactile', Empty)
+    #
+    # # Services can set tactile thresholds and enable tactile stops
+    # enable_tactile_stops = rospy.ServiceProxy('/reflex_takktile2/enable_tactile_stops', Empty)
+    # disable_tactile_stops = rospy.ServiceProxy('/reflex_takktile2/disable_tactile_stops', Empty)
+    # set_tactile_threshold = rospy.ServiceProxy('/reflex_takktile2/set_tactile_threshold', SetTactileThreshold)
+    #
+    # # This collection of publishers can be used to command the hand
+    # command_pub = rospy.Publisher('/reflex_takktile2/command', Command, queue_size=1)
+    # pos_pub = rospy.Publisher('/reflex_takktile2/command_position', PoseCommand, queue_size=1)
+    # vel_pub = rospy.Publisher('/reflex_takktile2/command_velocity', VelocityCommand, queue_size=1)
+    #
+    # rospy.Subscriber('/reflex_takktile2/hand_state', Hand, hand_state_cb)
+    # # raw_input("== When ready to calibrate the hand, press [Enter]\n")
+    # # calibrate_fingers()
+    # # raw_input("...\n")
+    # print("2")
+    # rate = rospy.Rate(20)
+    # while not rospy.is_shutdown():
+    #     pos_pub.publish(PoseCommand(2.7, 2.7, 2.7, 0.0))
+    #     rate.sleep()
+
+    # con.setPose((1.0, 1.0,1.0,1.0))
