514f0f0809e1209e804f6b27472c388f889ae20b
Fetch <liyuecheng27@gmail.com>
2020-01-26 17:01:31 -0600

    added gripper control physical mode

--- Motion/gripperController.py
+++ Motion/gripperController.py
@@ -50,12 +50,12 @@ class GripperController:
 
 
 
-
     def start(self):
         self.enable = True
         rospy.init_node('gripperController')
         controlThread = threading.Thread(target = self.controlLoop)
         controlThread.start()
+
 
     def controlLoop(self):
 
@@ -64,26 +64,27 @@ class GripperController:
         self.lastStateTime = float(now.secs) + 1e-9*float(now.nsecs) #get the ros time for each robot state msg
         rate = rospy.Rate(1.0/self.dt)
 
-        # self.calibrate()
         while not rospy.is_shutdown() and (not self.exit):
-            print(self.sense_finger_set)
+            # print(self.sense_finger_set)
 
             if self.stop:
-                self.pos_pub.publish(VelocityCommand(f1 = 0.0, f2 = 0.0, f3 = 0.0, preshape = 0.0))
+                self.pos_pub.publish(PoseCommand(f1 = 0.0, f2 = 0.0, f3 = 0.0, preshape = 0.0))
             elif self.command_type == "pose":
                 self.pos_pub.publish(PoseCommand(f1 = self.command_finger_set[0], f2 = self.command_finger_set[1], f3 = self.command_finger_set[2], preshape = self.command_finger_set[3]))
-                #self.command_type = None
             elif self.command_type == "velocity":
                 self.vel_pub.publish(VelocityCommand(f1 = self.command_finger_set[0], f2 = self.command_finger_set[1], f3 = self.command_finger_set[2], preshape = self.command_finger_set[3]))
-                # self.command_type = None
 
             rate.sleep()
+
+
     def callback(self, data):
         # self.lastStateTime = float(data.header.stamp.secs)+1e-9*float(data.header.stamp.nsecs)
         self.sense_finger_set[0] = data.motor[0].joint_angle
         self.sense_finger_set[1] = data.motor[1].joint_angle
         self.sense_finger_set[2] = data.motor[2].joint_angle
         self.sense_finger_set[3] = data.motor[3].joint_angle
+
+        self.contact = False
         for finger in data.finger:
             for sensor in finger.contact:
                 self.contact |= sensor
@@ -116,8 +117,6 @@ class GripperController:
 
 
     def setPose(self, position):
-        # now = rospy.get_rostime()
-        # currentTime = float(now.secs) + 1e-9*float(now.nsecs)
         self.regPose(position)
         self.command_finger_set[0] = position[0]
         self.command_finger_set[1] = position[1]
@@ -126,8 +125,6 @@ class GripperController:
         self.command_type = "pose"
 
     def setVelocity(self, velocity):
-        # now = rospy.get_rostime()
-        # currentTime = float(now.secs) + 1e-9*float(now.nsecs)
         self.regVelocity(velocity)
         self.command_finger_set[0] = velocity[0]
         self.command_finger_set[0] = velocity[1]
@@ -142,22 +139,32 @@ class GripperController:
         self.command_finger_set[3] = 1.0
         self.command_type = "pose"
 
+    def fclose(self):
+        self.close()
+        time.sleep(2)
+
     def open(self):
         self.command_finger_set[0] = 0.0
         self.command_finger_set[1] = 0.0
         self.command_finger_set[2] = 0.0
-        self.command_finger_set[3] = 1.0
+        self.command_finger_set[3] = 0.0
         self.command_type = "pose"
 
+    def fopen(self):
+        self.open()
+        time.sleep(2)
 
-####################################
-###  probably won't be used now  ###
-####################################
+
+
+#Calibrate will calibrate the finger and the sensor in the same time.
+#Calibration may move the fingers.
+#Also, calibrate sensor requires no contact
     def calibrate(self):
         rospy.wait_for_service('/reflex_takktile2/calibrate_fingers')
         calibrate_fingers = rospy.ServiceProxy('/reflex_takktile2/calibrate_fingers', Empty)
         rospy.wait_for_service('/reflex_takktile2/calibrate_tactile')
         calibrate_tactile = rospy.ServiceProxy('/reflex_takktile2/calibrate_tactile', Empty)
+        self.fopen()
         calibrate_fingers()
         calibrate_tactile()
 
@@ -171,13 +178,14 @@ class GripperController:
 
 
     def is_open(self):
-        print("is_open(): ", self.sense_finger_set)
-        if self.sense_finger_set[0] == 0.0 and self.sense_finger_set[1] == 0.0 and self.sense_finger_set[2] == 0.0:
-            return 1
+        # print("is_open(): ", self.sense_finger_set)
+        if self.sense_finger_set[0] - 0 < 0.1 and self.sense_finger_set[1] - 0 < 0.1 and self.sense_finger_set[2] - 0 < 0.1:
+            return True
         else:
-            return 0
+            return False
 
     def contact_with_object(self):
+        # print(self.contact)
         return self.contact
 
     def sensed_finger_positions(self):
@@ -185,9 +193,13 @@ class GripperController:
 
 
 
-    def stop(self):
+    def stop_process(self):
         self.enable = False
         self.stop = True
+
+    def fstop_process(self):
+        self.stop_process()
+        time.sleep(2)
 
     def resume(self):
         self.enable = True
@@ -206,7 +218,7 @@ class GripperController:
     def mark_read(self):
         self.new_state = False
 
-    def new_state(self):
+    def newState(self):
         return self.new_state
 
 def hand_state_cb(data):
@@ -217,40 +229,47 @@ def hand_state_cb(data):
 if __name__ == "__main__":
     con = GripperController()
     con.start()
+    con.setPose([2,2,0,1])
+    time.sleep(2)
     # while True:
     #     con.close()
     #     rospy.sleep(2)
     #     con.open()
     #     rospy.sleep(2)
     #     print(con.is_open())
-    con.setPose([0.0, 0.0, 0.0, 0.0])
-    con.sleep(1)
-    con.shutDown()
+    # time.sleep(3)
+    # con.calibrate()
+    #con.fclose()
+    #con.fopen()
+    # time.sleep(3)
+    # con.open()
+    # time.sleep(2)
+    # # print(con.is_open())
+    # # print(con.is_open())
+    # con.calibrate()
+    # while True:
+    #     print(con.contact_with_object())
+    # time.sleep(1)
+    # con.fopen()
     # con.setPose([1.0, 1.0, 1.0, 1.0])
-    # rospy.init_node('ExampleHandNode')
-    #
-    # # Services can automatically call hand calibration
-    # calibrate_fingers = rospy.ServiceProxy('/reflex_takktile2/calibrate_fingers', Empty)
-    # calibrate_tactile = rospy.ServiceProxy('/reflex_takktile2/calibrate_tactile', Empty)
-    #
-    # # Services can set tactile thresholds and enable tactile stops
-    # enable_tactile_stops = rospy.ServiceProxy('/reflex_takktile2/enable_tactile_stops', Empty)
-    # disable_tactile_stops = rospy.ServiceProxy('/reflex_takktile2/disable_tactile_stops', Empty)
-    # set_tactile_threshold = rospy.ServiceProxy('/reflex_takktile2/set_tactile_threshold', SetTactileThreshold)
-    #
-    # # This collection of publishers can be used to command the hand
-    # command_pub = rospy.Publisher('/reflex_takktile2/command', Command, queue_size=1)
-    # pos_pub = rospy.Publisher('/reflex_takktile2/command_position', PoseCommand, queue_size=1)
-    # vel_pub = rospy.Publisher('/reflex_takktile2/command_velocity', VelocityCommand, queue_size=1)
-    #
-    # rospy.Subscriber('/reflex_takktile2/hand_state', Hand, hand_state_cb)
-    # # raw_input("== When ready to calibrate the hand, press [Enter]\n")
-    # # calibrate_fingers()
-    # # raw_input("...\n")
-    # print("2")
-    # rate = rospy.Rate(20)
-    # while not rospy.is_shutdown():
-    #     pos_pub.publish(PoseCommand(2.7, 2.7, 2.7, 0.0))
-    #     rate.sleep()
-
-    # con.setPose((1.0, 1.0,1.0,1.0))
+    # time.sleep(2)
+    # con.fopen()
+    # con.setPose([2.0, 1.5, 1.0, 1.5])
+    # time.sleep(2)
+    # con.fopen()
+    # con.setPose([3.5, 3.5, 3.0, 2.0])
+    # time.sleep(2)
+    # con.fopen()
+    # con.setPose([3.5, 3.5, 1.0, 2.5])
+    # time.sleep(2)
+    # con.fopen()
+    # con.setPose([2.0, 2.0, 3.7, 3.0])
+    # time.sleep(2)
+    # con.fopen()
+    # con.setPose([3.0, 3.0, 3.0, 3.5])
+    # time.sleep(2)
+    # con.fstop_process()
+    # print(con.contact_with_object())
+    # print(con.sensed_finger_positions())
+    con.shutDown()
+    print("shutdown called")
