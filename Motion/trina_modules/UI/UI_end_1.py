632adda233c66c20f2cd8192ab633f795005a39c
eggshellboom <tli67@illinois.edu>
2020-06-06 23:46:26 -0500

    updated UI call examples and documents

--- Motion/trina_modules/UI/UI_end_1.py
+++ Motion/trina_modules/UI/UI_end_1.py
@@ -131,6 +131,23 @@ class MyQtMainWindow(QMainWindow):
         if reply == QMessageBox.Yes:
             self.global_state['collectRaySignal'] = [True,False]
             self.global_state['feedbackId']['getRayClick'] = id
+
+    def addButton(self):
+        name = self.rpc_args['name']
+        text = self.rpc_args['text']
+        id = '$' + name
+        self.buttons[name] = QPushButton(text)
+        self.buttons[name].clicked.connect(lambda: self._handleButtonClick(id))
+        self.leftLayout.addWidget(self.buttons[name])
+        self.server['UI_FEEDBACK'][str(id)] = {'REPLIED':True, 'MSG':False}
+
+    def _handleButtonClick(self,id):
+        self.server['UI_FEEDBACK'][str(id)] = {'REPLIED':True, 'MSG':True}
+
+
+
+
+
         
     def test(self):
         print("inde the test function")
@@ -178,6 +195,7 @@ class MyQtMainWindow(QMainWindow):
         self.glwidget.setParent(self.right)
         self.rightLayout.addWidget(self.glwidget)
     
+        self.buttons = {}
         self.welcomeText = QLabel("Welcome to TRINA UI! \n\n\nPlease Refer to Menu Bar for More Actions.")
         self.modeText = QLabel("")
         self.modeText.setWordWrap(True)
@@ -291,6 +309,8 @@ class UI_end_1:
         self.interface.initialize()
         self.server = KeyValueStore(self.interface)
         self.server["UI_STATE"] = {'controllerPositionState': {'leftController': {'controllerOrientation': [0.07739845663309097, -0.19212138652801514, 0.3228720426559448, 0.9235001802444458], 'controllerPosition': [-0.021801471710205078, -0.4208446145057678, 0.5902314186096191]}, 'rightController': {'controllerOrientation': [0.052883781492710114, 0.20788685977458954, -0.30593231320381165, 0.927573025226593], 'controllerPosition': [0.15437912940979004, -0.4229428172111511, 0.5827353000640869]}}, 'headSetPositionState': {'deviceRotation': [-0.027466144412755966, 0.7671623826026917, 0.003965826239436865, 0.6408524513244629]}, 'controllerButtonState': {'leftController': {'nearTouch': [False, False], 'press': [False, False, False, False], 'thumbstickMovement': [0.0, 0.0], 'touch': [False, False, False, False, False, False, False, False], 'squeeze': [0.0, 0.0]}, 'rightController': {'nearTouch': [False, False], 'press': [False, False, False, False], 'thumbstickMovement': [0.0, 0.0], 'touch': [False, False, False, False, False, False, False, False], 'squeeze': [0.0, 0.0]}}, 'UIlogicState': {'stop': False, 'autonomousMode': False, 'teleoperationMode': False}, 'title': 'UI Outputs'}
+        self.server["UI_END_COMMAND"] = []
+        self.server['UI_FEEDBACK'] = {}
         self.global_state = {'collectRaySignal':[False,False],'feedbackId':{'getRayClick':''}}
         self._serveVis()
 
